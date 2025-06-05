99dd8f64ddb19b4fbc899a25bd5b9a60fa47595e
aramind <88814622+aramind@users.noreply.github.com>
2025-06-05 16:44:22 +0800

    feat: adding the setup of index for be

---
+++ server/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+
+// env
+dotenv.config();
+const { PORT = 3001, MONGO_CONNECT: DB } = process.env;
+
+//routers
+
+// configs
+
+const app = express();
+
+// middlewares
+app.use([
+  helmet(),
+  express.json(),
+  express.urlencoded({ extended: true }),
+  morgan("combined"),
+  cookieParser(),
+]);
+
+// rendering the static file for the client
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+// base routes
+// authenticated routes
+
+// for the static site
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// if not found
+app.use((req, res) =>
+  res.status(404).json({ success: false, message: "Not found" })
+);
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(DB);
+    app.listen(PORT, () => console.log(`Server is listening to port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
