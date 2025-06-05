ce1035c6e8a749820f2ba006a2ac4e00999af46b
aramind <88814622+aramind@users.noreply.github.com>
2025-06-05 17:43:03 +0800

    feat (MAJOR) : adding verify player routes in be

--- server/src/controllers/auth/authController.js
+++ server/src/controllers/auth/authController.js
@@ -1,7 +1,9 @@
 const register = require("./register");
+const verify = require("./verify");
 
 const authController = {
   register,
+  verify,
 };
 
 module.exports = authController;
--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth/authController");
 const router = express.Router();
 
 router.post("/register", authController.register);
+router.post("/verify", authController.verify);
 
 module.exports = router;
