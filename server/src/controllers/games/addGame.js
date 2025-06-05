e95cd86c6f1204b16019383c64e9f78b6868acc0
aramind <88814622+aramind@users.noreply.github.com>
2025-06-05 18:58:42 +0800

    feat: adding controller to add game result

---
+++ server/src/controllers/games/addGame.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Game = require("../../models/Game");
+const Player = require("../../models/Player");
+
+const addGame = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { game, winner, loser, isDraw } = req.body;
+
+    const newGame = new Game(game);
+
+    if (isDraw) {
+      await Player.updateMany(
+        { _id: { $in: [game.playerR, game.playerM] } },
+        { $inc: { "stats.draws": 1 } },
+        { session }
+      );
+    } else if (winner && loser) {
+      await Player.findByIdAndUpdate(
+        winner,
+        { $inc: { "stats.wins": 1 } },
+        { session }
+      );
+      await Player.findByIdAndUpdate(
+        loser,
+        { $inc: { "stats.losses": 1 } },
+        { session }
+      );
+    } else {
+      await session.abortTransaction();
+      session.endSession();
+      return sendResponse.failed(res, "Invalid game result data", null, 400);
+    }
+
+    const savedGame = await newGame.save({ session });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return sendResponse.success(
+      res,
+      "Game and player stats updated successfully",
+      savedGame,
+      201
+    );
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    console.error(error);
+    sendResponse.failed(res, "Server Error: Adding Game", error, 500);
+  }
+};
+
+module.exports = addGame;
