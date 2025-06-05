2ee7887b5db84b000a0c49fc3c3c0a5378a24e28
aramind <88814622+aramind@users.noreply.github.com>
2025-06-05 16:56:18 +0800

    feat: adding game model schema

---
+++ server/src/models/Game.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Player = require("./Player");
+
+const GameSchema = new mongoose.Schema(
+  {
+    playerR: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: Player,
+      required: true,
+    },
+    playerM: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: Player,
+      required: true,
+    },
+    displayNames: {
+      R: { type: String, required: true },
+      M: { type: String, required: true },
+    },
+    board: {
+      content: {
+        type: [String],
+        default: ["", "", "", "", "", "", "", "", ""],
+      },
+    },
+    winner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: Player,
+      default: null,
+    },
+    isDraw: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Game", GameSchema);
