19628ffac75e1dca4afbce53bc602b062b949d43
aramind <88814622+aramind@users.noreply.github.com>
2025-06-05 20:48:03 +0800

    feat: making the landing hero section responsive to small screens

--- client/src/pages/landing/Title.jsx
+++ client/src/pages/landing/Title.jsx
@@ -1,10 +1,18 @@
 import { Box, Typography } from "@mui/material";
+import useIsInMobile from "../../hooks/useIsInMobile";
 import React from "react";
 
 const Title = () => {
+  const isInMobile = useIsInMobile();
   return (
-    <Box marginBottom={1}>
-      <Typography variant="h1">RM GAME</Typography>
+    <Box marginBottom={1} marginX="auto">
+      <Typography
+        variant="h1"
+        textAlign="center"
+        fontSize={isInMobile ? "4rem" : null}
+      >
+        RM GAME
+      </Typography>
     </Box>
   );
 };
