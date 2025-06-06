f2952f79d788c5446647bc4a1decb3f549490228
aramind <88814622+aramind@users.noreply.github.com>
2025-06-06 21:24:43 +0800

    refactor: removing in landing page display of game history

--- client/src/pages/landing/Landing.jsx
+++ client/src/pages/landing/Landing.jsx
@@ -1,13 +1,11 @@
-import { Box, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import React from "react";
 import HeroSection from "./HeroSection";
-import GameHistory from "../history/GameHistory";
 
 const Landing = () => {
   return (
     <Stack>
       <HeroSection />
-      <GameHistory />
     </Stack>
   );
 };
