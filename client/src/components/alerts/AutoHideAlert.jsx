aae28bb2fdc5c689fcff1d9f8804590daf8a8f40
aramind <88814622+aramind@users.noreply.github.com>
2025-06-07 09:02:37 +0800

    refactor: positioning the minor notifications diff for mobile and large screens

--- client/src/components/alerts/AutoHideAlert.jsx
+++ client/src/components/alerts/AutoHideAlert.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useGlobalState } from "../../context/GlobalStateProvider";
 import { Alert, Snackbar } from "@mui/material";
+import useIsInMobile from "../../hooks/useIsInMobile";
 
 const AutoHideAlert = () => {
+  const isInMobile = useIsInMobile();
   const {
     globalState: { alert },
     dispatch,
@@ -21,13 +23,20 @@ const AutoHideAlert = () => {
       open={alert.open}
       onClose={handleClose}
       autoHideDuration={alert.autoHideDuration}
-      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      anchorOrigin={{
+        vertical: isInMobile ? "top" : "bottom",
+        horizontal: isInMobile ? "center" : "left",
+      }}
       sx={{ zIndex: "999999" }}
     >
       <Alert
         severity={alert.severity}
         onClose={handleClose}
-        sx={{ display: "flex", alignItems: "center" }}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          fontSize: { xs: "0.7rem", md: "0.9rem" },
+        }}
       >
         {alert.message}
       </Alert>
