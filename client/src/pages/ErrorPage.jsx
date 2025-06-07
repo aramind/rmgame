6d997b17687ec46bad20091624a0399f6df93c65
aramind <88814622+aramind@users.noreply.github.com>
2025-06-07 09:13:14 +0800

    feat: adding a temporary error page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import NavBar from "../components/navbar/NavBar";
+import { Button, Stack, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  const navigate = useNavigate();
+  return (
+    <Stack
+      pt="80px"
+      width={{ xs: "90vw", md: "80vw" }}
+      marginX="auto"
+      minHeight="100vh"
+      className="centered"
+    >
+      <NavBar />
+      <Typography textAlign="center">
+        Oops! That wasn’t supposed to happen. 😅
+      </Typography>
+      <Typography textAlign="center">
+        But don’t worry — it should work fine next time!
+      </Typography>
+      <br />
+      <Button variant="contained" onClick={() => navigate(-1)}>
+        RETURN
+      </Button>
+    </Stack>
+  );
 };
 
 export default ErrorPage;
